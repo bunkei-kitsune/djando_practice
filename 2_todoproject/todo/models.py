f566ea17fbc78fc385861ef9aaf57b80dc766559
Ai Hayakawa <ai.kks0214@gmail.com>
2022-06-25 12:34:09 +0900

    models.pyファイルでフィールド(どのようなデータを扱うか)を指定

--- 2_todoproject/todo/models.py
+++ 2_todoproject/todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# どのようなデータを扱うかを指定(フィールドの指定)
+# Todomodelにはtitleとmemoという2つの情報を入れていく
+class Todomodel(models.Model):
+    # todoモデルのオブジェクト(1つ1つのデータ)を作るとき、タイトルをまず入れるからtitile
+    # CharFieldは大・小サイズの文字列情報を扱うフィールド
+    title = models.CharField(max_length=100) # 引数max_lengthは必ず指定
+    # 次にメモを書くのでmemo
+    # TextFieldは多量のテキストを扱うフィールド
+    memo = models.TextField()
